bc14154756381a0e92f62c6e96d719f7702a3b70
Ramkumar Ramalingam <rramalingam@planittesting.com>
2017-06-21 05:40:24 +0100

    First commit

---
+++ src/main/java/com/planit/sample/samples/Solution.java
@@ -0,0 +1,22 @@
+package com.planit.sample.samples;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCaseNum = sc.nextInt();
+        for (int i = 0; i < testCaseNum; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+            int num = a;
+            for (int j = 0; j < n; j++) {
+                num += b*Math.pow(2, j);
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
