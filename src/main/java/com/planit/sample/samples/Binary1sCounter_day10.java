90e239edd0e994f5a4f9daa13f58ff3c2067b4db
Ramkumar Ramalingam <rramalingam@planittesting.com>
2017-06-22 22:24:03 +0100

    Test

---
+++ src/main/java/com/planit/sample/samples/Binary1sCounter_day10.java
@@ -0,0 +1,10 @@
+package com.planit.sample.samples;
+
+public class Binary1sCounter_day10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/main/java/com/planit/sample/samples/test.java
+++ src/main/java/com/planit/sample/samples/test.java
@@ -5,14 +5,23 @@ import java.util.Scanner;
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-Scanner in = new Scanner(System.in);
-		
-		int L=in.nextInt();
-		
-	//	for (int i=1; i<=L;i++){fact=fact*i}
+		Scanner sc = new Scanner(System.in);
 
+		int n = sc.nextInt();
+
+		String binary = Integer.toBinaryString(n);
+		int maxConsecutiveOneNum = 0;
+		int consecutiveOneNum = 0;
+		for (int i = 0; i < binary.length(); i++) {
+			if (binary.charAt(i) == '1') {
+				consecutiveOneNum++;
+				maxConsecutiveOneNum = Math.max(maxConsecutiveOneNum, consecutiveOneNum);
+			} else {
+				consecutiveOneNum = 0;
+			}
+		}
+		System.out.println(maxConsecutiveOneNum);
+
+		sc.close();
 	}
-
 }
